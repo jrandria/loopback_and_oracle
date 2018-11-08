3b7354ce1d03bff5daf99ba8350509298cc5cf5d
Randria72 <nocry72@gmail.com>
2018-11-08 08:39:58 +0100

    Modification Test Automigration Oracle

--- server/bin/automigration.1.js
+++ server/bin/automigration.1.js
@@ -3,12 +3,16 @@ var app = require('../server');
 // this loads the accountDb configuration in ~/server/datasources.json
 var dataSource = app.dataSources.db;
 
-var lbTables = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role', 'meetup'];
-dataSource.automigrate(lbTables, function(er) {
-    if (er) throw er;
-    console.log('Loopback tables [' + lbTables + '] created in ', dataSource.adapter.name);
-    dataSource.disconnect();
-  });
+dataSource.automigrate('User', function(err) {
+  if (err) throw err;
+  dataSource.disconnect();
+});
+
+// this automigrates the model
+dataSource.automigrate('meetup', function(err) {
+  if (err) throw err;
+  dataSource.disconnect();
+});
 
 
 /*
